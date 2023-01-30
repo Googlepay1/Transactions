f2c5fa8c18f9ea73f603b9e98c6a2288c0e7b507
Adithyasai123 <adithyaacgacg3@gmail.com>
2023-01-30 11:38:17 -0500

    First code commit

---
+++ primeornot.java
@@ -0,0 +1,18 @@
+#This is example of existing string
+#Here we use "isnumeric" before if condition because we retriew only digits from the string
+
+str= "I am 25 years old"
+b =" "
+for i in str:
+    if i.isnumeric():
+       b+=i
+print(b)
+
+#Here in this case we are taking input from user
+str= input("Enter the string/line :")
+b =" "
+for i in str:
+    if i.isnumeric():
+       b+=i
+print(b)
+
